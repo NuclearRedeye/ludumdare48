84a57853c6914a62c2500b54c9b7eab2379a21bf
NuclearRedeye <github@mneah.com>
2021-04-24 10:30:59 +0100

    feat: add an initial canvas for rendering

---
+++ src/ts/config.ts
@@ -0,0 +1,2 @@
+export const canvasWidth: number = 800;
+export const canvasHeight: number = 600;
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,10 @@
 import { createTextElement } from './utils/utils.js';
+import { canvasWidth, canvasHeight } from './config.js';
 
 window.onload = function (): void {
+  const canvas: HTMLCanvasElement = document.createElement('canvas');
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  document.body.appendChild(canvas);
   document.body.appendChild(createTextElement('Created by NuclearRedeye'));
 };
