651ffabbe084c55e2e17015d75fe4df436d8d7b9
NuclearRedeye <github@mneah.com>
2021-04-24 12:08:12 +0100

    chore: add a basic main loop

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,66 @@
 import { createTextElement } from './utils/utils.js';
+import { Mark, getCurrentFramesPerSecond, getDelta, getElapsed } from './utils/frametimer.js';
 import { canvasWidth, canvasHeight } from './config.js';
 
+// Globals
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+
+// States
+let pause: boolean = false;
+let debug: boolean = false;
+
+// Main Loop
+function onTick(timestamp: number): void {
+  if (!pause) {
+    // Mark the timer
+    Mark(timestamp);
+
+    // Clear the Canvas, although no real need.
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    // If 'debug' is enabled, print various stats.
+    if (debug) {
+      context.fillStyle = 'white';
+      context.fillText(`Current FPS: ${getCurrentFramesPerSecond().toFixed(2)}`, 10, 10);
+      context.fillText(`Previous FT: ${getDelta().toFixed(2)}`, 10, 30);
+      context.fillText(`Runtime: ${getElapsed().toFixed(2)} seconds`, 10, 50);
+    }
+  }
+
+  window.requestAnimationFrame(onTick);
+}
+
+window.onkeydown = (event: KeyboardEvent): void => {
+  switch (event.code) {
+    default:
+      break;
+  }
+};
+
+window.onkeyup = (event: KeyboardEvent): void => {
+  switch (event.code) {
+    // Toogle pausing the mainloop
+    case 'KeyP':
+      pause = !pause;
+      break;
+
+    // Toggle debug on or off
+    case 'KeyD':
+      debug = !debug;
+      break;
+
+    default:
+      break;
+  }
+};
+
 window.onload = function (): void {
-  const canvas: HTMLCanvasElement = document.createElement('canvas');
+  canvas = document.createElement('canvas') as HTMLCanvasElement;
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
+  context = canvas.getContext('2d', { alpha: false }) as CanvasRenderingContext2D;
   document.body.appendChild(canvas);
   document.body.appendChild(createTextElement('Created by NuclearRedeye'));
+  window.requestAnimationFrame(onTick);
 };
---
+++ src/ts/utils/frametimer.ts
@@ -0,0 +1,23 @@
+let start: number;
+let current: number;
+let previous: number;
+
+export function Mark(timestamp: number): void {
+  if (start === undefined) {
+    start = timestamp;
+  }
+  previous = current;
+  current = timestamp - start;
+}
+
+export function getElapsed(): number {
+  return current;
+}
+
+export function getDelta(): number {
+  return current - previous;
+}
+
+export function getCurrentFramesPerSecond(): number {
+  return 1000 / (current - previous);
+}
