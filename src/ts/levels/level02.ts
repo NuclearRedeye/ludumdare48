8dddb0cf513d6218d0243539a417bd090758510e
NuclearRedeye <github@mneah.com>
2021-04-25 11:27:09 +0100

    fix: linter warning

--- src/ts/levels/level02.ts
+++ src/ts/levels/level02.ts
@@ -1,5 +1,5 @@
 import { Level } from '../interfaces/level';
-import { createFloor as f, createWall as w, createEntrance as e, createExit as g } from '../utils/cell-utils.js';
+import { createFloor as f, createWall as w, createEntrance as e } from '../utils/cell-utils.js';
 
 export const level02: Level = {
   depth: 2,
