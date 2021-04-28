b8856180d8417da6798a0e6225cbc7671bc86abd
NuclearRedeye <github@mneah.com>
2021-04-28 17:41:02 +0100

    chore: add method for collision checking entities and cells

--- src/ts/utils/collision-utils.ts
+++ src/ts/utils/collision-utils.ts
@@ -1,4 +1,5 @@
 import { Entity } from '../interfaces/entity';
+import { Rectangle } from '../interfaces/rectangle';
 
 export function checkEntityCollision(a: Entity, b: Entity): boolean {
   let retVal = false;
@@ -10,4 +11,35 @@ export function checkEntityCollision(a: Entity, b: Entity): boolean {
     retVal = true;
   }
   return retVal;
+}
+
+export function checkEntityWithCell(entity: Entity, rectangle: Rectangle): boolean {
+  // temporarectangle.y variables to set edges for testing
+  let testX = entity.x;
+  let testY = entity.y;
+
+  // Calculate which side is closest on the X axis.
+  if (entity.x < rectangle.x) {
+    testX = rectangle.x;
+  } else if (entity.x > rectangle.x + rectangle.width) {
+    testX = rectangle.x + rectangle.width;
+  }
+
+  // Calculate which side is closest on the Y axis.
+  if (entity.y < rectangle.y) {
+    testY = rectangle.y;
+  } else if (entity.y > rectangle.y + rectangle.height) {
+    testY = rectangle.y + rectangle.height;
+  }
+
+  // get distance from closest edges
+  const distX = entity.x - testX;
+  const distY = entity.y - testY;
+  const distance = Math.sqrt(distX * distX + distY * distY);
+
+  // if the distance is less than the entity.radius, collision!
+  if (distance <= entity.radius) {
+    return true;
+  }
+  return false;
 }
